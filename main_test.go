f9b9be3eaf86f8533da34176cb47a81d849ab1e7
Jordan Anderson <me@jba.io>
2016-01-02 04:34:30 -0600

    test tweaks, to try and catch bugs

--- main_test.go
+++ main_test.go
@@ -7,16 +7,39 @@ import (
 
 func TestMarkdownRender(t *testing.T) {
     // Read raw Markdown
-    rawmdf := "./test.md"
+    rawmdf := "./tests/test.md"
     rawmd, err := ioutil.ReadFile(rawmdf)	
     if err != nil {
 		t.Error("Unable to access test.md")
     }
     // Read what rendered Markdown HTML should look like
-    rendermdf := "./test.html"
+    rendermdf := "./tests/test.html"
     rendermd, err := ioutil.ReadFile(rendermdf)
     if err != nil {
 		t.Error("Unable to access test.html")
+    }
+    // []byte to string
+    rendermds := string(rendermd)
+    
+	rawmds := markdownRender(rawmd)
+    if rawmds != rendermds {
+        t.Error("Converted Markdown does not equal readymade test")
+    }
+}
+
+// Tests a corner case where stuff without markdown wasn't being rendered
+func TestMarkdownRender2(t *testing.T) {
+    // Read raw Markdown
+    rawmdf := "./tests/test2.md"
+    rawmd, err := ioutil.ReadFile(rawmdf)	
+    if err != nil {
+		t.Error("Unable to access test2.md")
+    }
+    // Read what rendered Markdown HTML should look like
+    rendermdf := "./tests/test2.html"
+    rendermd, err := ioutil.ReadFile(rendermdf)
+    if err != nil {
+		t.Error("Unable to access test2.html")
     }
     // []byte to string
     rendermds := string(rendermd)
