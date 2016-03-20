f69fb21e84943ad9e077235c14319d284e5f67f2
Jordan Anderson <me@jba.io>
2016-03-19 21:36:57 -0500

    Mostly working using Picnic

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,87 +1,4 @@
 $(document).ready(function(){
-  $("#imageup").submit(function(event){
-    event.preventDefault();
-    $.ajax( {
-      url: '/api/image/new',
-      type: 'POST',
-      data: new FormData( this ),
-      processData: false,
-      contentType: false,
-      success: function(data){
-        if(data.success){
-          $(".alerts").append("<div class=\"alert-box success\" data-alert>Image successfully uploaded! <a style='color:#fff' href=/i/"+data.name+"><i class='fa fa-external-link'></i>[Image link]</a><a class=\"close\">&times;</a></div>");
-          $("#imageup")[0].reset();
-        $(document).foundation('alert','reflow');
-        } else {
-          $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed upload<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
-        }
-      }
-    });
-  });
-  $("#remoteimageup").submit(function(event){
-    event.preventDefault();
-	  $.post( "/api/image/remote", $( this ).serialize(), function(data){
-        if(data.success){
-          $(".alerts").append("<div class=\"alert-box success\" data-alert>Image successfully uploaded! <a style='color:#fff' href=/i/"+data.name+"><i class='fa fa-external-link'></i>[Image link]</a><a class=\"close\">&times;</a></div>");
-          $("#remoteimageup")[0].reset();
-        $(document).foundation('alert','reflow');
-        } else {
-          $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed upload<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
-        }
-	  });
-  });  
-
-  $("#fileup").submit(function(event){
-    event.preventDefault();
-    $.ajax( {
-      url: '/api/file/new',
-      type: 'POST',
-      data: new FormData( this ),
-      processData: false,
-      contentType: false,
-      success: function(data){
-        if(data.success){
-          $(".alerts").append("<div class=\"alert-box success\" data-alert>File successfully uploaded! <a style='color:#fff' href=/d/"+data.name+"><i class='fa fa-external-link'></i>[File link]</a><a class=\"close\">&times;</a></div>");
-          $("#fileup")[0].reset();
-          $(document).foundation('alert','reflow');
-        } else {
-          $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed upload<a class=\"close\">&times;</a></div>");
-          $(document).foundation('alert','reflow');
-        }
-      }
-    });
-  });
-  $("#remotefileup").submit(function(event){
-    event.preventDefault();
-    $.post( "/api/file/remote", $( this ).serialize(), function(data){
-        if(data.success){
-          $(".alerts").append("<div class=\"alert-box success\" data-alert>File successfully uploaded! <a style='color:#fff' href=/d/"+data.name+"><i class='fa fa-external-link'></i>[File link]</a><a class=\"close\">&times;</a></div>");
-          $("#remotefileup")[0].reset();
-        $(document).foundation('alert','reflow');
-        } else {
-          $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed upload<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
-        }
-    });
-  }); 
-
-  $("#shorturl").submit(function(event){
-    event.preventDefault();  
-    console.log( $( this ).serialize() ); 
-    $.post( "/api/shorten/new", $( this ).serialize(), function(data){
-        if(data.success){
-          $(".alerts").append("<div class=\"alert-box success\" data-alert>Link successfully shortened! <a style='color:#fff' href="+data.name+"><i class='fa fa-external-link'></i>[Short URL]</a><a class=\"close\">&times;</a></div>");
-          $("#shorturl")[0].reset();
-        $(document).foundation('alert','reflow');
-        } else {
-          $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed to shorten URL<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
-        }
-      });
-  });
-
   $("#savewiki").submit(function(event){
     event.preventDefault();
     //var editortxt = document.getElementsByName("editor")[0].value;
@@ -96,25 +13,14 @@ $(document).ready(function(){
           //$("#savewiki")[0].reset();
           //$(document).foundation('alert','reflow');
         } else {
-          $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed to save wiki page<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
+            $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed to save wiki page<a class=\"close\">&times;</a></div>");
+            $("#alerts").append("<article><header><h3>Failed to save wiki page</h3>");
+            $("#alerts").append("<label for=\"alert_modal\" class=\"close\">&times;</label></header>");
+            $("#alerts").append("<section class=\"content\">Please check logs or try again.</section>");
+            $("#alerts").append("<footer><label for=\"alert_modal\" class=\"button dangerous\">Okay</label></footer></article>");
+            $( "#alert_modal" ).prop( "checked", true );
         }
       });
-  });  
-
-  $("a.delete").click(function(event){
-    event.preventDefault();
-    var link = $( this ).attr("href");
-    //console.log( link );
-    $.get( link, function(data){
-        if(data.success){
-          $(".alerts").append("<div class=\"alert-box success\" data-alert>Successfully deleted "+data.name+"!<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
-        } else {
-          $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed to delete<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
-        }
-    });    
   });
 
   $("#login").submit(function(event){
@@ -126,8 +32,12 @@ $(document).ready(function(){
         //$("#login-form").remove();
         //$(document).foundation('alert','reflow');
       } else {
-        $(".alerts").append("<div class=\"alert-box alert\" data-alert>Failed login<a class=\"close\">&times;</a></div>");
-        $(document).foundation('alert','reflow');
+        $( "#login_modal" ).prop( "checked", false );
+        $("#alerts").append("<article><header><h3>Login failure</h3>");
+        $("#alerts").append("<label for=\"alert_modal\" class=\"close\">&times;</label></header>");
+        $("#alerts").append("<section class=\"content\">Please try to login again.</section>");
+        $("#alerts").append("<footer><label for=\"alert_modal\" class=\"button dangerous\">Okay</label></footer></article>");
+        $( "#alert_modal" ).prop( "checked", true );
       }
     });
   });
