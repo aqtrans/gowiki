de9fdf5ccbbc234b5ec5fc99571056e47ef4515e
Jordan Anderson <me@jba.io>
2016-06-19 05:00:47 -0500

    Update tests to work with new Markdown options

--- main_test.go
+++ main_test.go
@@ -1,135 +1,135 @@
 package main
 
 import (
-    "testing"
-    "io/ioutil"
-    "io"
-    "bytes"
-    //"github.com/drewolson/testflight"
-    "net/http/httptest"
-    "net/http"
-    //"github.com/gorilla/context"
-    //"fmt"
-    //"log"
-    "net/url"
-    //"os"
-    //"jba.io/go/auth"
-    "strings"
-    "github.com/gorilla/mux"
-    //"github.com/stretchr/testify/assert"
+	"bytes"
+	"io"
+	"io/ioutil"
+	"testing"
+	//"github.com/drewolson/testflight"
+	"net/http"
+	"net/http/httptest"
+	//"github.com/gorilla/context"
+	//"fmt"
+	//"log"
+	"net/url"
+	//"os"
+	//"jba.io/go/auth"
+	"strings"
+
+	"github.com/gorilla/mux"
+	//"github.com/stretchr/testify/assert"
 )
 
 type key int
-const UserKey  key = 1
-const RoleKey  key = 2
+
+const UserKey key = 1
+const RoleKey key = 2
 
 var (
-    server   *httptest.Server
-    reader   io.Reader //Ignore this for now
-    serverUrl string
-    m   *mux.Router
-    req *http.Request
-    rr *httptest.ResponseRecorder
+	server    *httptest.Server
+	reader    io.Reader //Ignore this for now
+	serverUrl string
+	m         *mux.Router
+	req       *http.Request
+	rr        *httptest.ResponseRecorder
 )
 
-
 func setup() {
-    /*
-    // Open and initialize auth database
-    // Retrieve a temporary path.
-    f, err := ioutil.TempFile("", "")
-    if err != nil {
-        panic(err)
-    }
-    path := f.Name()
-    
-    auth.Open(path)
-    auth.AuthDbInit()
-    
+	/*
+	   // Open and initialize auth database
+	   // Retrieve a temporary path.
+	   f, err := ioutil.TempFile("", "")
+	   if err != nil {
+	       panic(err)
+	   }
+	   path := f.Name()
 
-    //mux router with added question routes
-    m = mux.NewRouter()
-    m.HandleFunc("/new", newHandler)
-    //Router(m)
-    server = httptest.NewServer(Router(m))
-    serverUrl = server.URL
-    */
-    //The response recorder used to record HTTP responses
-    rr = httptest.NewRecorder()
+	   auth.Open(path)
+	   auth.AuthDbInit()
+
+
+	   //mux router with added question routes
+	   m = mux.NewRouter()
+	   m.HandleFunc("/new", newHandler)
+	   //Router(m)
+	   server = httptest.NewServer(Router(m))
+	   serverUrl = server.URL
+	*/
+	//The response recorder used to record HTTP responses
+	rr = httptest.NewRecorder()
 }
 
-
 func TestHealthCheckHandler(t *testing.T) {
-    //assert := assert.New(t)
-    setup()
-    // Create a request to pass to our handler. We don't have any query parameters for now, so we'll
-    // pass 'nil' as the third parameter.
-    req, err := http.NewRequest("GET", "/health", nil)
-    if err != nil {
-        t.Fatal(err)
-    }
+	//assert := assert.New(t)
+	setup()
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", "/health", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-    handler := http.HandlerFunc(HealthCheckHandler)
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	handler := http.HandlerFunc(HealthCheckHandler)
 
-    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method 
-    // directly and pass in our Request and ResponseRecorder.
-    handler.ServeHTTP(rr, req)
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
 
-    // Check the status code is what we expect.
-    if status := rr.Code; status != http.StatusOK {
-        t.Errorf("handler returned wrong status code: got %v want %v",
-            status, http.StatusOK)
-    }
-    //assert.Equal(rr.Code, http.StatusOK, "HealthCheckHandler not returning 200")
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	//assert.Equal(rr.Code, http.StatusOK, "HealthCheckHandler not returning 200")
 
-    // Check the response body is what we expect.
-    expected := `{"alive": true}`
-    if rr.Body.String() != expected {
-        t.Errorf("handler returned unexpected body: got %v want %v",
-            rr.Body.String(), expected)
-    }
+	// Check the response body is what we expect.
+	expected := `{"alive": true}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
 
 func TestNewHandler(t *testing.T) {
-    setup()
+	setup()
 
-    // Create a request to pass to our handler.
-    form := url.Values{}
-    form.Add("newwiki", "omg/yeah/what")
-    //log.Println(form)
-    reader = strings.NewReader(form.Encode())
-    req, err := http.NewRequest("POST", "/new", reader)
-    if err != nil {
-        t.Fatal(err)
-    }
-    //log.Println(reader)
-    
-    req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	// Create a request to pass to our handler.
+	form := url.Values{}
+	form.Add("newwiki", "omg/yeah/what")
+	//log.Println(form)
+	reader = strings.NewReader(form.Encode())
+	req, err := http.NewRequest("POST", "/new", reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	//log.Println(reader)
 
-    // We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-    handler := http.HandlerFunc(newHandler)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-    // Our handlers satisfy http.Handler, so we can call their ServeHTTP method 
-    // directly and pass in our Request and ResponseRecorder.
-    handler.ServeHTTP(rr, req)
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	handler := http.HandlerFunc(newHandler)
 
-    //log.Println(rr.Header())
-    
-    // Check the status code is what we expect.
-    if status := rr.Code; status != http.StatusTemporaryRedirect {
-        t.Errorf("handler returned wrong status code: got %v want %v",
-            status, http.StatusCreated)
-    }
-    
-    //log.Println(rr.Header().Get("Location"))
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
 
-    // Check the response body is what we expect.
-    expected := `/omg/yeah/what?a=edit`
-    if rr.Header().Get("Location") != expected {
-        t.Errorf("handler returned unexpected body: got %v want %v",
-            rr.Body.String(), expected)
-    }
+	//log.Println(rr.Header())
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != http.StatusTemporaryRedirect {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+
+	//log.Println(rr.Header().Get("Location"))
+
+	// Check the response body is what we expect.
+	expected := `/omg/yeah/what?a=edit`
+	if rr.Header().Get("Location") != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 
 }
 
@@ -142,7 +142,7 @@ func setup() {
 
 
 func TestSetUser(t *testing.T) {
-    
+
     userJson := url.Values{"newwiki": {"omg/yeah/stuff"}}
     reader = strings.NewReader(userJson.Encode())
     r, err := http.NewRequest("GET", serverUrl+"/new", reader)
@@ -152,14 +152,14 @@ func TestSetUser(t *testing.T) {
 
     r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
     auth.SetContext(r, "admin", "Admin", "")
-    
+
     w, err := http.DefaultClient.Do(r)
 
     if err != nil {
         log.Println(err) //Something is wrong while sending request
     }
     log.Println(context.GetAll(r))
-    
+
     if w.StatusCode != http.StatusCreated {
         t.Errorf("Success expected: %d %s", w.StatusCode, w.Header) //Uh-oh this means our test failed
     }
@@ -198,15 +198,15 @@ func (db *TestDB) Close() {
 
 
 func TestCreateUser(t *testing.T) {
-    
+
     //setUser()
-    
+
     db := NewTestDB()
-    defer db.Close()    
+    defer db.Close()
 
     //server = httptest.NewServer(Router())
     //router := Router()
-    //w := httptest.NewRecorder()   
+    //w := httptest.NewRecorder()
 
     userJson := `{"newwiki": "omg-yeah/omg/omg"}`
 
@@ -223,12 +223,12 @@ func TestCreateUser(t *testing.T) {
     if err != nil {
         t.Error(err) //Something is wrong while sending request
     }
-    
+
     if w.StatusCode != http.StatusCreated {
         log.Println(context.GetAll(r))
         t.Errorf("Success expected: %d %s", w.StatusCode, w.Header) //Uh-oh this means our test failed
     }
-    
+
 }
 */
 /*
@@ -244,14 +244,14 @@ func testUserEnvMiddle(next http.Handler) http.Handler {
 
 func TestNewHandler(t *testing.T) {
     setup()
-    
+
     userJson := url.Values{"newwiki": {"omg/yeah/stuff"}}
     reader = strings.NewReader(userJson.Encode())
-    
+
     req, _ = http.NewRequest("POST", serverUrl+"/new", reader)
-    
+
     req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-    
+
     m.ServeHTTP(rr, req)
     log.Println(rr.Code)
     log.Println(rr.Header())
@@ -259,129 +259,129 @@ func TestNewHandler(t *testing.T) {
 */
 
 func TestMarkdownRender(t *testing.T) {
-    // Read raw Markdown
-    rawmdf := "./tests/test.md"
-    rawmd, err := ioutil.ReadFile(rawmdf)	
-    if err != nil {
+	// Read raw Markdown
+	rawmdf := "./tests/test.md"
+	rawmd, err := ioutil.ReadFile(rawmdf)
+	if err != nil {
 		t.Error("Unable to access test.md")
-    }
-    // Read what rendered Markdown HTML should look like
-    rendermdf := "./tests/test.html"
-    rendermd, err := ioutil.ReadFile(rendermdf)
-    if err != nil {
+	}
+	// Read what rendered Markdown HTML should look like
+	rendermdf := "./tests/test.html"
+	rendermd, err := ioutil.ReadFile(rendermdf)
+	if err != nil {
 		t.Error("Unable to access test.html")
-    }
-    // []byte to string
-    rendermds := string(rendermd)
-    
+	}
+	// []byte to string
+	rendermds := string(rendermd)
+
 	rawmds := markdownRender(rawmd)
-    if rawmds != rendermds {
-        t.Error("Converted Markdown does not equal test")
-    }
+	if rawmds != rendermds {
+		//ioutil.WriteFile("./tests/test.html", []byte(rawmds), 0755)
+		t.Error("Converted Markdown does not equal test" + "\n Raw: \n" + rawmds + "Rendered: \n" + rendermds)
+	}
 }
 
 // Tests a corner case where stuff without markdown wasn't being rendered
 func TestMarkdownRender2(t *testing.T) {
-    // Read raw Markdown
-    rawmdf := "./tests/test2.md"
-    rawmd, err := ioutil.ReadFile(rawmdf)	
-    if err != nil {
+	// Read raw Markdown
+	rawmdf := "./tests/test2.md"
+	rawmd, err := ioutil.ReadFile(rawmdf)
+	if err != nil {
 		t.Error("Unable to access test2.md")
-    }
-    // Read what rendered Markdown HTML should look like
-    rendermdf := "./tests/test2.html"
-    rendermd, err := ioutil.ReadFile(rendermdf)
-    if err != nil {
+	}
+	// Read what rendered Markdown HTML should look like
+	rendermdf := "./tests/test2.html"
+	rendermd, err := ioutil.ReadFile(rendermdf)
+	if err != nil {
 		t.Error("Unable to access test2.html")
-    }
-    // []byte to string
-    rendermds := string(rendermd)
-    
+	}
+	// []byte to string
+	rendermds := string(rendermd)
+
 	rawmds := markdownRender(rawmd)
-    if rawmds != rendermds {
-        t.Error("Converted Markdown does not equal test2")
-    }
+	if rawmds != rendermds {
+		//ioutil.WriteFile("./tests/test2.html", []byte(rawmds), 0755)
+		t.Error("Converted Markdown does not equal test2" + "\n Raw: \n" + rawmds + "Rendered: \n" + rendermds)
+	}
 }
 
 // Below is for testing the difference between just writing the Tags string directly as fed in from the wiki form, or using a []string as the source, but having to write them using a for loop
-// Results, seems string is the best bet for now: 
+// Results, seems string is the best bet for now:
 //    BenchmarkBufferString-4    10000            996527 ns/op
 //    BenchmarkBufferArray-4      1000           1651610 ns/op
 
-
 var title string = "YEAH BENCHMARK OMG"
 var name string = "WOOHOO"
-var tagsarray = []string{"OMG","YEAH","WHAT","ZZZZ","FFFF","EEEE","RRRTRT","GRHTH","GBHFT","QPFLG","MGJHIB","LRIGJB","DJCUDK","WIFJV","GKBIBK","XKSDFM","RUFJS","SLDKF","ZKDJF","WIFKFG","EIFLG","DKFIBJ","WWRKG","SLFIBK","PRIVATE"}
+var tagsarray = []string{"OMG", "YEAH", "WHAT", "ZZZZ", "FFFF", "EEEE", "RRRTRT", "GRHTH", "GBHFT", "QPFLG", "MGJHIB", "LRIGJB", "DJCUDK", "WIFJV", "GKBIBK", "XKSDFM", "RUFJS", "SLDKF", "ZKDJF", "WIFKFG", "EIFLG", "DKFIBJ", "WWRKG", "SLFIBK", "PRIVATE"}
 var tagsstring string = "OMG, YEAH, WHAT, ZZZZ, FFFF, EEEE, RRRTRT, GRHTH, GBHFT, QPFLG, MGJHIB, LRIGJB, DJCUDK, WIFJV, GKBIBK, XKSDFM, RUFJS, SLDKF ,ZKDJF, WIFKFG, EIFLG, DKFIBJ, WWRKG, SLFIBK, PRIVATE"
 var body string = "WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO\n# OMG \n # YEAH"
 
 func BenchmarkBufferString(b *testing.B) {
-    for n := 0; n < b.N; n++ {
-        var buffer bytes.Buffer
-        buffer.WriteString("---\n")
-        if title == "" {
-            title = name
-        }
-        buffer.WriteString("title: "+ title)
-        buffer.WriteString("\n")
-        if tagsstring != "" {
-            buffer.WriteString("tags: "+ tagsstring)
-            buffer.WriteString("\n")
-        }
-        buffer.WriteString("---\n")
-        buffer.WriteString(body)
-        body = buffer.String()
-    }
+	for n := 0; n < b.N; n++ {
+		var buffer bytes.Buffer
+		buffer.WriteString("---\n")
+		if title == "" {
+			title = name
+		}
+		buffer.WriteString("title: " + title)
+		buffer.WriteString("\n")
+		if tagsstring != "" {
+			buffer.WriteString("tags: " + tagsstring)
+			buffer.WriteString("\n")
+		}
+		buffer.WriteString("---\n")
+		buffer.WriteString(body)
+		body = buffer.String()
+	}
 }
 
 func BenchmarkBufferArray(b *testing.B) {
-    for n := 0; n < b.N; n++ {
-        var buffer bytes.Buffer
-        buffer.WriteString("---\n")
-        if title == "" {
-            title = name
-        }
-        buffer.WriteString("title: "+ title)
-        buffer.WriteString("\n")
-        if tagsarray != nil {
-            buffer.WriteString("tags: [")
-            for _, v := range tagsarray {
-                buffer.WriteString(v + " ")
-            }
-            buffer.WriteString("]")
-            buffer.WriteString("\n")
-        }
-        buffer.WriteString("---\n")
-        buffer.WriteString(body)
-        body = buffer.String()
-    }
+	for n := 0; n < b.N; n++ {
+		var buffer bytes.Buffer
+		buffer.WriteString("---\n")
+		if title == "" {
+			title = name
+		}
+		buffer.WriteString("title: " + title)
+		buffer.WriteString("\n")
+		if tagsarray != nil {
+			buffer.WriteString("tags: [")
+			for _, v := range tagsarray {
+				buffer.WriteString(v + " ")
+			}
+			buffer.WriteString("]")
+			buffer.WriteString("\n")
+		}
+		buffer.WriteString("---\n")
+		buffer.WriteString(body)
+		body = buffer.String()
+	}
 }
-
 
 // Below is for testing the difference between sorting through a []string and creating a []string using SplitString, then sorting through it
 
 func benchmarkIsPrivate(size int, b *testing.B) {
-    list := "testing"
-    n := size
-    for i := 0; i < n; i++ {
-        list = " " + list
-    }
-    //tags := strings.Split(list, " ")
-    for n := 0; n < b.N; n++ {
-        isPrivate(list)
-    }
+	list := "testing"
+	n := size
+	for i := 0; i < n; i++ {
+		list = " " + list
+	}
+	//tags := strings.Split(list, " ")
+	for n := 0; n < b.N; n++ {
+		isPrivate(list)
+	}
 }
 
 func benchmarkIsPrivateArray(size int, b *testing.B) {
-    list := []string{"testing"}
-    n := size
-    for i := 0; i < n; i++ {
-        list = append(list, "testing")
-    }
-    //tags := strings.Split(list, " ")
-    for n := 0; n < b.N; n++ {
-        isPrivateA(list)
-    }
+	list := []string{"testing"}
+	n := size
+	for i := 0; i < n; i++ {
+		list = append(list, "testing")
+	}
+	//tags := strings.Split(list, " ")
+	for n := 0; n < b.N; n++ {
+		isPrivateA(list)
+	}
 }
 
 //func BenchmarkIsPrivate2(b *testing.B) { benchmarkIsPrivate(2, b) }
@@ -396,10 +396,8 @@ func benchmarkIsPrivateArray(size int, b *testing.B) {
 //func BenchmarkIsPrivate1000(b *testing.B) { benchmarkIsPrivate(1000, b) }
 //func BenchmarkIsPrivateArray1000(b *testing.B) { benchmarkIsPrivateArray(1000, b) }
 
-func BenchmarkIsPrivate10000(b *testing.B) { benchmarkIsPrivate(10000, b) }
+func BenchmarkIsPrivate10000(b *testing.B)      { benchmarkIsPrivate(10000, b) }
 func BenchmarkIsPrivateArray10000(b *testing.B) { benchmarkIsPrivateArray(10000, b) }
 
 //func BenchmarkIsPrivate100000(b *testing.B) { benchmarkIsPrivate(100000, b) }
 //func BenchmarkIsPrivateArray100000(b *testing.B) { benchmarkIsPrivateArray(100000, b) }
-
-
