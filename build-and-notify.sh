a139e1bc52744f1c4de1e65094bb35d7363f13c0
Jordan Anderson <me@jba.io>
2016-07-04 10:34:34 -0500

    build script now fetches newest CSS before building

--- build-and-notify.sh
+++ build-and-notify.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-notify-send -i gopher.png -a Golang 'Golang compiled'
+wget -O assets/bourbon.css.map http://static.jba.io/bourbon.css.map
+wget -O assets/bourbon.css http://static.jba.io/bourbon.css
+go build -o ./wiki && notify-send -i gopher.png -a Golang 'Golang compiled'
 ./wiki -d
