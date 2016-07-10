0eb1ebf7fe21baa1116fc9ee46f77709265aaa0a
Jordan Anderson <me@jba.io>
2016-07-09 22:26:39 -0500

    No longer retrieve CSS, because it should be in place when built locally

--- build-and-notify.sh
+++ build-and-notify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -O assets/bourbon.css.map http://static.jba.io/wiki.css.map
-wget -O assets/bourbon.css http://static.jba.io/wiki.css
+#wget -O assets/bourbon.css.map http://static.jba.io/wiki.css.map
+#wget -O assets/bourbon.css http://static.jba.io/wiki.css
 go build -o ./wiki && notify-send -i gopher.png -a Golang 'Golang compiled'
 ./wiki -d
