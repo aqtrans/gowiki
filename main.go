8543eec6694b18aecf4dfdaa698d6cda2e24998c
Jordan Anderson <me@jba.io>
2017-04-15 18:38:04 -0500

    Properly cancel the main context

--- main.go
+++ main.go
@@ -3324,7 +3324,8 @@ func main() {
 	log.Println("Shutting down server...")
 
 	// shut down gracefully, but wait no longer than 5 seconds before halting
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	srv.Shutdown(ctx)
 
 	log.Println("Server gracefully stopped")
