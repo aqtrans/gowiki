b3c1e45b21f1673be79e60735c9c117152a11b56
Jordan Anderson <me@jba.io>
2017-05-07 01:11:18 -0500

    Try and fix git commit --author flag.

--- main.go
+++ main.go
@@ -731,7 +731,7 @@ func gitAddFilepath(filepath string) error {
 
 // Execute `git commit -m {msg}` in workingDirectory
 func gitCommitWithMessage(msg string) error {
-	o, err := gitCommand("commit", "--author='Golang Wiki <>'", "-m", msg).CombinedOutput()
+	o, err := gitCommand("commit", "--author", "'Golang Wiki <>'", "-m", msg).CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error during `git commit`: %s\n%s", err.Error(), string(o))
 	}
@@ -741,7 +741,7 @@ func gitCommitWithMessage(msg string) error {
 
 // Execute `git commit -m "commit from GoWiki"` in workingDirectory
 func gitCommitEmpty() error {
-	o, err := gitCommand("commit", "--author='Golang Wiki <>'", "-m", "commit from GoWiki").CombinedOutput()
+	o, err := gitCommand("commit", "--author", "'Golang Wiki <>'", "-m", "commit from GoWiki").CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error during `git commit`: %s\n%s", err.Error(), string(o))
 	}
