2599c62f99ddb43881cb4903bad94692c25186b3
Jordan Anderson <me@jba.io>
2017-08-09 13:19:03 -0500

    Correct min-css gulpfile

--- scss/gulpfile.js
+++ scss/gulpfile.js
@@ -6,13 +6,9 @@ gulp.task('sass', function() {
   return gulp.src('wiki.scss')
     .pipe(sourcemaps.init())
     .pipe($.sass({
-      includePaths: sassPaths,
       outputStyle: 'compressed' // if css compressed **file size**
     })
       .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('../assets/css/'));
 });
