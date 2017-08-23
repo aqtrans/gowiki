7b1109d04c470845d65b98a9fc175a82f2266d2b
Jordan Anderson <me@jba.io>
2017-08-23 15:46:47 -0500

    Try to eliminate need for bower

--- scss/gulpfile.js
+++ scss/gulpfile.js
@@ -3,25 +3,25 @@ var $    = require('gulp-load-plugins')();
 var sourcemaps = require('gulp-sourcemaps');
 
 var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+  'node_modules/foundation-sites/scss',
+  'node_modules/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
-  return gulp.src('wiki.scss')
+  return gulp.src('scss/wiki.scss')
     .pipe(sourcemaps.init())
     .pipe($.sass({
       includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
+      outputStyle: 'nested' // if css compressed **file size**
     })
       .on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('../assets/css/'));
+    .pipe(gulp.dest('assets/css/'));
 });
 
 gulp.task('default', ['sass'], function() {
-  gulp.watch(['*.scss'], ['sass']);
+  gulp.watch(['scss/*.scss'], ['sass']);
 });
