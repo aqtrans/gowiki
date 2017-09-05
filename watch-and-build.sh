16961e1a44d4a69c1dba593a263ac6443645965a
Jordan Anderson <me@jba.io>
2017-09-05 15:57:16 -0500

    Added a config file for reflex (github.com/cespare/reflex), allowing gulp-like watch and build functionality. Also updated respective watch-and-build.sh script.

--- watch-and-build.sh
+++ watch-and-build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-CompileDaemon -exclude-dir=md -exclude-dir=md2 -exclude-dir=.git -exclude-dir=vendor -include="*.tmpl" -command="./wiki -d"
+#CompileDaemon -exclude-dir=md -exclude-dir=md2 -exclude-dir=.git -exclude-dir=vendor -include="*.tmpl" -command="./wiki -d"
+go get github.com/cespare/reflex
+reflex -c reflex.conf
