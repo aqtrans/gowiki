0eb1fe72264098cbddafb95725c0c60cb50c7ff5
Jordan Anderson <me@jba.io>
2017-09-08 12:06:33 -0500

    Now using modd to watch-and-build instead of entr, reflex, or CompileDaemon.

--- watch-and-build.sh
+++ watch-and-build.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+## Using CompileDaemon, only building main.go:
 #CompileDaemon -exclude-dir=md -exclude-dir=md2 -exclude-dir=.git -exclude-dir=vendor -include="*.tmpl" -command="./wiki -d"
+
+## Using reflex:
 #go get github.com/cespare/reflex
 #reflex -c reflex.conf
-#ls scss/*.scss | entr sass scss/grid.scss assets/css/wiki.css &&
-## Using Entr (http://entrproject.org/) now, as reflex wasn't working properly on OSX
+
+## Using Entr (http://entrproject.org/), as reflex wasn't working properly on OSX
+#ls scss/*.scss | entr sass scss/grid.scss assets/css/wiki.css
 #### Rebuilding SASS and Go in one
-ls -d main.go main_test.go templates/** scss/grid.scss | entr -r sh -c 'sass scss/grid.scss assets/css/wiki.css && go run main.go'
+#ls -d main.go main_test.go templates/** scss/grid.scss | entr -r sh -c 'sass scss/grid.scss assets/css/wiki.css && go run main.go'
+
+## Using Modd (https://github.com/cortesi/modd) now, which allows multiple commands, multiple matches
+go get github.com/cortesi/modd 
+modd 
