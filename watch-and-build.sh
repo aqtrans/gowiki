849a0a8e3686720124590fd6b4b120a2491b0b2f
Jordan Anderson <me@jba.io>
2017-09-08 01:53:43 -0500

    watch-and-build.sh now using entr, rebuilds both SASS and main.go.

--- watch-and-build.sh
+++ watch-and-build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 #CompileDaemon -exclude-dir=md -exclude-dir=md2 -exclude-dir=.git -exclude-dir=vendor -include="*.tmpl" -command="./wiki -d"
-go get github.com/cespare/reflex
-reflex -c reflex.conf
+#go get github.com/cespare/reflex
+#reflex -c reflex.conf
+#ls scss/*.scss | entr sass scss/grid.scss assets/css/wiki.css &&
+## Using Entr (http://entrproject.org/) now, as reflex wasn't working properly on OSX
+#### Rebuilding SASS and Go in one
+ls -d main.go main_test.go templates/** scss/grid.scss | entr -r sh -c 'sass scss/grid.scss assets/css/wiki.css && go run main.go'
