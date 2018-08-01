26404cd22de9eef38b2c26dc476d8b9bc9ea043e
Jordan Anderson <me@jba.io>
2018-07-31 20:52:05 -0500

    Dev and prod working with vfsgen. Trick is setting the Templates/Assets variable separately. moved SVG funcs to assets subpkg.

--- vfs/templates/utils.go
+++ vfs/templates/utils.go
@@ -2,42 +2,20 @@ package templates
 
 import (
 	"html/template"
-	"io/ioutil"
-	"log"
 	"strings"
+
+	"jba.io/go/wiki/vfs/assets"
 )
 
 func typeIcon(gitType string) template.HTML {
 	var html template.HTML
 	if gitType == "blob" {
-		html = svg("file-text")
+		html = assets.Svg("file-text")
 	}
 	if gitType == "tree" {
-		html = svg("folder-open")
+		html = assets.Svg("folder-open")
 	}
 	return html
-}
-
-func svg(iconName string) template.HTML {
-	// MAJOR TODO:
-	// Check for file existence before trying to read the file; if non-existent return ""
-	iconFile, err := ioutil.ReadFile("assets/icons/" + iconName + ".svg")
-	if err != nil {
-		log.Println("Error loading assets/icons/", iconName, err)
-		return template.HTML("")
-	}
-	return template.HTML(`<div class="svg-icon">` + string(iconFile) + `</div>`)
-}
-
-func svgByte(iconName string) []byte {
-	// MAJOR TODO:
-	// Check for file existence before trying to read the file; if non-existent return ""
-	iconFile, err := ioutil.ReadFile("assets/icons/" + iconName + ".svg")
-	if err != nil {
-		log.Println("Error loading assets/icons/", iconName, err)
-		return []byte("")
-	}
-	return []byte(`<div class="svg-icon">` + string(iconFile) + `</div>`)
 }
 
 func isLoggedIn(s string) bool {
