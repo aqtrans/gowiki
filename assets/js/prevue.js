7f6f427e1133eb0076e886c6f1d2a6b6d91af9b8
Jordan Anderson <me@jba.io>
2019-04-08 13:15:07 -0500

    Working VueJS tabs on edit!

--- assets/js/prevue.js
+++ assets/js/prevue.js
@@ -1,3 +1,47 @@
+// Tabs based on https://codepen.io/samrose3/pen/wgaYEz
+Vue.component('tabs', {
+  template: `
+    <div>
+      <ul class="tabs sub" id="subtabs">
+        <li v-for="tab in tabs" :class="[{ 'is-active': tab.isActive }, 'tab-title']">
+          <a class="tablinks" @click="selectTab(tab)">{{ tab.name }}</a>
+        </li>
+      </ul>
+
+      <div class="tabs-details">
+        <slot></slot>
+      </div>
+    </div>
+  `,
+  data() {
+    return { tabs: [] }
+  },
+  created() {
+    this.tabs = this.$children;
+  },
+  methods: {
+    selectTab: function(selectedTab) {
+      this.tabs.forEach(tab => {
+        tab.isActive = (tab.name == selectedTab.name);
+      });
+    }
+  }
+});
+
+Vue.component('tab', {
+  props: {
+    name: { required: true },
+    selected: { default: false }
+  },
+  template: `<div v-show="isActive"><slot></slot></div>`,
+  data() {
+    return { isActive: false }
+  },
+  mounted() {
+    this.isActive = this.selected;
+  }
+});
+
 function mdPreview(text) {
   /*
     var csrf = document.getElementsByName("gorilla.csrf.Token")[0].value;
@@ -44,7 +88,7 @@ function mdPreview(text) {
 }
 
 var editVue = new Vue({
-    el: '#tab-content',
+    el: '#tabs',
     data: {
         input: document.getElementById("wikieditor").value
     },
@@ -103,7 +147,7 @@ var editVue = new Vue({
     }
   })
 
-
+/*
   var tabs = [
     {
       name: 'Edit', 
@@ -133,3 +177,4 @@ var tabs = new Vue({
     currentTab: tabs[0]
   }
 })
+*/
