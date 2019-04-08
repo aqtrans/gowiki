2fc7ae68badbce41745d6ac2f0c1fcc6a2f5e35c
Jordan Anderson <me@jba.io>
2019-04-08 13:41:04 -0500

    Vue tabs moved to it's own JS file, included on all pages

--- assets/js/prevue.js
+++ assets/js/prevue.js
@@ -1,47 +1,3 @@
-// Tabs based on https://codepen.io/samrose3/pen/wgaYEz
-Vue.component('tabs', {
-  template: `
-    <div>
-      <ul class="tabs sub" id="subtabs">
-        <li v-for="tab in tabs" :class="[{ 'is-active': tab.isActive }, 'tab-title']">
-          <a class="tablinks" @click="selectTab(tab)">{{ tab.name }}</a>
-        </li>
-      </ul>
-
-      <div class="tabs-details">
-        <slot></slot>
-      </div>
-    </div>
-  `,
-  data() {
-    return { tabs: [] }
-  },
-  created() {
-    this.tabs = this.$children;
-  },
-  methods: {
-    selectTab: function(selectedTab) {
-      this.tabs.forEach(tab => {
-        tab.isActive = (tab.name == selectedTab.name);
-      });
-    }
-  }
-});
-
-Vue.component('tab', {
-  props: {
-    name: { required: true },
-    selected: { default: false }
-  },
-  template: `<div v-show="isActive"><slot></slot></div>`,
-  data() {
-    return { isActive: false }
-  },
-  mounted() {
-    this.isActive = this.selected;
-  }
-});
-
 function mdPreview(text) {
   /*
     var csrf = document.getElementsByName("gorilla.csrf.Token")[0].value;
@@ -145,7 +101,7 @@ var editVue = new Vue({
         this.input = e.target.value
       }, 300)
     }
-  })
+});
 
 /*
   var tabs = [
