d2ac131d37f0ebc8086fb29649113cbf9411cf50
Jordan Anderson <me@jba.io>
2019-12-10 22:19:27 -0600

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+function build_css()
+{
+    sassc -M scss/grid.scss assets/css/wiki.css
+}
+
+while [ "$1" != "" ]; do 
+    case $1 in
+        run)
+            GO111MODULE=on go generate ./...
+            build_css
+            GO111MODULE=on go run -race .
+            ;;
+        css)
+            build_css
+            exit
+            ;;
+        build)
+            GO111MODULE=on go generate ./...
+            build_css
+            GO111MODULE=on go build -o gowiki .
+            exit
+            ;;
+        build-pkg)
+            GO111MODULE=on go generate ./...
+            build_css
+            GO111MODULE=on go build -o gowiki
+            debuild -us -uc -b
+            exit
+            ;;
+    esac
+done
