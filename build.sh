a5e329318b29109d8ef6e014dc5143de16294518
Jordan Anderson <me@jba.io>
2020-08-09 01:25:30 -0500

    Add vfs disabled run option to build.sh

--- build.sh
+++ build.sh
@@ -10,22 +10,27 @@ function build_css()
 while [ "$1" != "" ]; do 
     case $1 in
         run)
-            GO111MODULE=on go generate ./...
+            GO111MODULE=on go generate
+            build_css
+            GO111MODULE=on go run -race -tags=dev .
+            ;;
+        run-prod)
+            GO111MODULE=on go generate
             build_css
             GO111MODULE=on go run -race .
-            ;;
+            ;;            
         css)
             build_css
             exit
             ;;
         build)
-            GO111MODULE=on go generate ./...
+            GO111MODULE=on go generate
             build_css
             GO111MODULE=on go build -o gowiki .
             exit
             ;;
         build-pkg)
-            GO111MODULE=on go generate ./...
+            GO111MODULE=on go generate
             build_css
             GO111MODULE=on go build -o gowiki
             debuild -us -uc -b
