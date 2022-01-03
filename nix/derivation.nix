7f6753b2a1323dd9df54e4e1ee845a6ab560efa1
Jordan Anderson <me@jba.io>
2022-01-03 11:27:33 -0600

    Add appVersion git sha1 back in and build CSS inside Nix

--- nix/derivation.nix
+++ nix/derivation.nix
@@ -1,14 +1,23 @@
-{ stdenv, lib, buildGoModule, git, makeWrapper, substituteAll }:
+{ stdenv, lib, buildGoModule, git, makeWrapper, substituteAll, sassc, runCommand }:
 
 buildGoModule rec {
-  name = "gowiki";
+  pname = "gowiki";
   #version = "0.0.1";
+  
+  # dynamic version based on git; https://blog.replit.com/nix_dynamic_version
+  revision = runCommand "get-rev" {
+          nativeBuildInputs = [ git ];
+      } "GIT_DIR=${src}/.git git rev-parse --short HEAD | tr -d '\n' > $out";  
+
+  version = "0" + builtins.readFile revision;
 
   src = ../.;
 
-  nativeBuildInputs = [ makeWrapper ];
+  nativeBuildInputs = [ makeWrapper sassc ];
 
   buildInputs = [ git ];
+
+  ldflags = [ "-X main.appVersion=${version}" ];
 
   vendorSha256 = "0hbx4z3i33sprs3n5j86q9sahxq1zvi46mic4nj98wyhmhhqsmxa";
 
@@ -19,6 +28,7 @@ buildGoModule rec {
   subPackages = [ "./" ];
 
   preBuild = ''
+    sassc -t compressed -M scss/grid.scss assets/css/wiki.css
     substituteInPlace main.go --replace 'gitPath, err := exec.LookPath("git")' 'gitPath, err := exec.LookPath("${git}/bin/git")'
     substituteInPlace main_test.go --replace 'gitPath, err := exec.LookPath("git")' 'gitPath, err := exec.LookPath("${git}/bin/git")'
   '';
